07caac27dafe5f119a5bc0d9180204c83585b46c
getdome <ux.getappeasy@gmail.com>
2018-12-27 22:15:44 +0700

    First push from macmini

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: getappeasyux
+ * Date: 2018-12-21
+ * Time: 23:22
+ */
---
+++ phpinfo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: getappeasyux
+ * Date: 2018-12-21
+ * Time: 23:38
+ */
+phpinfo();
+?>
